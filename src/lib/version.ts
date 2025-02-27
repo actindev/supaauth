fa1c2ee14a2b1d12933f34efe5a3e053529ff9a0
Gitmaxd <131803031+Gitmaxd@users.noreply.github.com>
2025-02-26 17:24:48 -0700

    Bump version to 0.1.18

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,3 +1,3 @@
 // This file is auto-generated. Do not edit directly.
-// Generated on 2025-02-27T00:11:02.612Z
-export const APP_VERSION = '0.1.17';
+// Generated on 2025-02-27T00:24:42.461Z
+export const APP_VERSION = '0.1.18';
